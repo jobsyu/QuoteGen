e9f11b06fde11d64bf5c063bba219a09d684f0de
於程鹏 <yuchengpeng@yuchengpengdeMac.local>
2015-03-19 04:47:30 -0700

    Initial Commit

---
+++ QuoteGen/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QuoteGen
+//
+//  Created by 於程鹏 on 15-3-19.
+//  Copyright (c) 2015年 於程鹏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QuoteGen/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QuoteGen
+//
+//  Created by 於程鹏 on 15-3-19.
+//  Copyright (c) 2015年 於程鹏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
